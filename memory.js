89e3acb2b7727dacc3cae051b218f0cdd9238c65
Gene Olafsen <engineering@strattonassociates.com>
2023-06-19 22:14:12 -0400

    polar park
    and added carousel for images

--- memory.js
+++ memory.js
@@ -276,6 +276,18 @@
             }
         }
 
+		function carousel() {
+		  var i;
+		  var x = document.getElementsByClassName("mySlides");
+		  for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";  
+		  }
+		  myIndex++;
+		  if (myIndex > x.length) {myIndex = 1}    
+		  x[myIndex-1].style.display = "block";  
+		  setTimeout(carousel, 2000); // Change image every 2 seconds
+		}
+		
         //$('field').on('click', 'div.card', dispatchClick);
         $('nav').on('click', 'div.navlink', dispatchNav);
         $('nav').on('mouseover', 'div.navlink', function(event, element) {
@@ -335,6 +347,9 @@
         shuffle();
 
         Object.extend(object, { shuffle: shuffle, win: win });
+		
+		var myIndex = 0; 
+		carousel();
 		
     });
 
